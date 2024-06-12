5c321cd79348fa983939ead4b1b52654fb1dad4e
ktk <ktk@ktks-MacBook-Air.local>
2024-06-12 17:28:45 +0530

    feat - added build dist

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,17 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const db_1 = __importDefault(require("./db"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)());
 (0, db_1.default)();
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
-});
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
-});
+app.use("/api", productRoutes_1.default);
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
